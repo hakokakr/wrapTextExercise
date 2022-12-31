8c25e6a49badd9894f680f163a2ff332a7b834b0
hakokakr <hakokakr@jyu.fi>
2023-01-01 00:58:07 +0200

    Reworked the wrapText and made new tests

--- harjoitus1/src/HarjoitteluTS.ts
+++ harjoitus1/src/HarjoitteluTS.ts
@@ -1,74 +1,90 @@
+let myText = `Turpis in eu mi bibendum neque egestas congue quisque.
+Should - wrap - this - even - if - there - is - hyphens -
+Aliquam etiam erat velit scelerisque in dictum.`;
+let myNum = 25;
+console.log(wrapText(myText, myNum).s);
+console.log(wrapText(myText, myNum).modified);
+
 /**
- * Wrap the given text to lines of max n characters splitting from space.
- * Doesn't modify code block or TeX block contents.
+ * Wraps given text to lines, to maximum of given n characters.
+ * Doesn't modify code block contents, TeX block contents, code lines (lines starting with 4 spaces). 
+ * 
+ * @param text - Text to be wrapped
+ * @param maxLineLength - Maximum line length
+ * @param wrapStart - Wrapping start point (might be useful to give the cursor position[in characters])
+ * @returns Was there any modifications & possibly wrapped text
  */
-function wrapText(s: string, n: number) {
-    if (n <= 0) {
-        return {modified: false, s: s};
+function wrapText(text: string, maxLineLength: number, wrapStart: number = 0) {
+    // If the given max line length is not desired: don't modify
+    if (maxLineLength <= 0) {
+        return {modified: false, s: text};
     }
-    let rx: RegExp = /(```[^```]*```|\$\$[^\$\$]*\$\$)/g; // block contents that shouldn't be modified
-    const sepRxLines = s.split(rx);
-    let lines = [];
-    let nextToRx: Boolean = false;
-    for (let i = 0; i < sepRxLines.length; i++) {
-        let element: string = sepRxLines[i];
-        if (!element.match(rx)) { 
-            if (nextToRx && element.indexOf("\n") == 0) { 
-                element = element.slice(1); 
-            }
-            element.split("\n").forEach(piece => {
-                lines.push(piece);
-            }); 
-            nextToRx = false;
-            continue;
-        }
-        if (lines[lines.length - 1] == "") { lines.pop(); }
-        lines.push(element);
-        nextToRx = true;
-    } // splits by line break, ignores empty lines around blocks
-    let needJoin = false;
-    for (let i = 0; i < lines.length; i++) {
-        let line = lines[i];
-        let sep = "";
-        if (line.length > n) {
-            lines[i] = "";
-            while (true) {
-                let ignore = false
-                let p = -1;
-                ignore = !!line.match(rx)
-                if (line.length > n && !ignore) {
-                    p = line.lastIndexOf(" ", n);
-                    if (p < 0) {
-                        p = line.indexOf(" ");
-                    } // long line
-                }
-                if (ignore) { 
-                    sep = "";
-                }
-                if (p < 0) {
-                    lines[i] += sep + line;
-                    break;
-                } // line is as short as it needs
-                lines[i] += sep + line.substring(0, p);
-                line = line.substring(p + 1);
-                if (
-                    i + 1 < lines.length &&
-                    lines[i + 1].length > 0 &&
-                    !" 0123456789-".includes(lines[i + 1][0])
-                    ) {
-                        lines.splice(i + 1, 0, line);
-                        needJoin = true;
-                        break;
-                    } // remaining line placed to the next index
-                sep = "\n";
-                needJoin = true;
-            }
+
+    // Saves everything before wrap starting point
+    let result = "" + text.substring(0, wrapStart);
+    let modified = false;
+
+    // Contents that shouldn't be modified
+    let rx: RegExp = /(```[^```]*```|\$\$[^\$\$]*\$\$|~~~[^~~~]*~~~|^    .*|^\s*-.*)/gm;
+
+    // Splits text into wrappable and non-wrappable blocks
+    const sections = text.substring(wrapStart).split(rx);
+
+    // Loops through all blocks
+    for (const section of sections) {
+        if (rx.test(section)) {
+            // Adds contents that shouldn't be modified directly into result
+            result += section;
+        } else {
+            // Wraps the text if needed and adds to result
+            const wrappedSection = wrapSection(section, maxLineLength);
+            result += wrappedSection.s;
+            // Keeps track of modifications
+            modified = modified || wrappedSection.modified;
         }
     }
-    if (needJoin) {
-        return {modified: true, s: lines.join("\n")};
+
+    // Returns boolean and text
+    return {modified: modified, s: result};
+}
+
+/**
+ * Wraps all the given text to a maximum of given number of characters.
+ * Doesn't split long words.
+ * 
+ * @param text - Text to be wrapped
+ * @param maxLineLength - Maximum line length
+ * @returns Was there any modifications & possibly wrapped text
+ */
+function wrapSection(text: string, maxLineLength: number) {
+    let modified = false;
+    let currentLine = "";
+    let result = "";
+    let sep = "";
+
+    // Breaks wrappable block into words
+    const words = text.split(" ");
+
+    // Loops through the words
+    for (const word of words) {
+        // Checks if adding a word to the current line exceeds the max line length
+        if (currentLine.length + word.length > maxLineLength) {
+            // Exceeds the max line length: adds current line to result with line break & clears current line
+            result += currentLine + "\n";
+            currentLine = "";
+            sep = "";
+            modified = true;
+        }
+        // Adds the word to current line
+        currentLine += sep + word;
+        sep = " ";
     }
-    return {modified: false, s: s};
+
+    // Adds the last line to result
+    result += currentLine;
+
+    // Returns boolean and text
+    return {modified: modified, s: result};
 }
 
 module.exports = wrapText;
--- harjoitus1/tests/harjoitteluTS.test.js
+++ harjoitus1/tests/harjoitteluTS.test.js
@@ -66,13 +66,76 @@ describe("wrapText", () => {
      }); 
 
 
+     it("Shouldn't modify code block", () => {
+        // assertions
+        var givenNumber = 10;
+        var testString = 
+`\`\`\`
+code code code code code code code code
+\`\`\``;
+        var textShouldBe = testString.slice();
+        var modifiedShouldBe = false;
+        // test
+        var result = wrapText(testString, givenNumber);
+        expect(result.s).toBe(textShouldBe);
+        expect(result.modified).toBe(modifiedShouldBe);
+     }); 
+
+
+     it("Shouldn't modify TeX block", () => {
+        // assertions
+        var givenNumber = 10;
+        var testString = 
+`$$
+math math math math math math math math
+$$`;
+        var textShouldBe = testString.slice();
+        var modifiedShouldBe = false;
+        // test
+        var result = wrapText(testString, givenNumber);
+        expect(result.s).toBe(textShouldBe);
+        expect(result.modified).toBe(modifiedShouldBe);
+     }); 
+
+
+     it("Shouldn't modify code line", () => {
+        // assertions
+        var givenNumber = 10;
+        var testString = `    code code code code code code code code`;
+        var textShouldBe = testString.slice();
+        var modifiedShouldBe = false;
+        // test
+        var result = wrapText(testString, givenNumber);
+        expect(result.s).toBe(textShouldBe);
+        expect(result.modified).toBe(modifiedShouldBe);
+     }); 
+
+
+     it("Shouldn't modify the list", () => {
+        // assertions
+        var givenNumber = 10;
+        var testString = 
+`- text text text text text
+   - text
+   - text
+- text text text text text
+   - text`;
+        var textShouldBe = testString.slice();
+        var modifiedShouldBe = false;
+        // test
+        var result = wrapText(testString, givenNumber);
+        expect(result.s).toBe(textShouldBe);
+        expect(result.modified).toBe(modifiedShouldBe);
+     }); 
+
+
      it("Should wrap the given text to maximum of given number of characters", () => {
         // assertions
         var givenNumber = 75;
-var testString = 
+        var testString = 
 `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas maecenas pharetra convallis posuere morbi. Libero enim sed faucibus turpis in eu mi bibendum. Etiam erat velit scelerisque in dictum. Id leo in vitae turpis massa sed elementum.`
-// long text without any line breaks or code blocks
-var textShouldBe = 
+        // long text without any line breaks or code blocks
+        var textShouldBe = 
 `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Turpis egestas maecenas
 pharetra convallis posuere morbi. Libero enim sed faucibus turpis in eu mi
@@ -89,14 +152,14 @@ massa sed elementum.`
      it("Shouldn't wrap the long code block", () => {
         // assertions
         var givenNumber = 20;
-var testString = 
+        var testString = 
 `Lorem ipsum
 \`\`\`
     print("hello world hello world hello world")
 \`\`\`
 Lorem ipsum`
-// short text with line breaks and with long code block in the middle of the plain text
-var textShouldBe = testString.slice();
+        // short text with line breaks and with long code block in the middle of the plain text
+        var textShouldBe = testString.slice();
         var modifiedShouldBe = false;
         // test
         var result = wrapText(testString, givenNumber);
@@ -108,14 +171,14 @@ var textShouldBe = testString.slice();
      it("Shouldn't modify anything, because includes a codeblock and text is written without line breaks before code block", () => {
         // assertions
         var givenNumber = 20;
-var testString = 
+        var testString = 
 `Lorem ipsum
 \`\`\`
     print("hello world hello world hello world")
 \`\`\`
 Lorem ipsum`
-// editor doesn't consider this as a codeblock, user's editing might be in progress
-var textShouldBe = testString.slice();
+        // editor doesn't consider this as a codeblock, user's editing might be in progress
+        var textShouldBe = testString.slice();
         var modifiedShouldBe = false;
         // test
         var result = wrapText(testString, givenNumber);
@@ -123,107 +186,10 @@ var textShouldBe = testString.slice();
         expect(result.modified).toBe(modifiedShouldBe);
      }); 
 
-
-     it("Shouldn't wrap code block insides, but should wrap the texts before and after it", () => {
-        // assertions
-        var givenNumber = 12;
-var testString = 
-`Lorem ipsum
-Lorem ipsum
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum
-Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum`
-// text with line breaks and with long code block in the middle of the plain text
-var textShouldBe = 
-`Lorem ipsum
-Lorem ipsum
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum
-Lorem ipsum
-lorem ipsum
-lorem ipsum
-lorem ipsum
-lorem ipsum`
-        var modifiedShouldBe = true;
-        // test
-        var result = wrapText(testString, givenNumber);
-        expect(result.s).toBe(textShouldBe);
-        expect(result.modified).toBe(modifiedShouldBe);
-     }); 
-
-
-     it("Shouldn't wrap code block insides, but should wrap the texts before and after it", () => {
-        // assertions
-        var givenNumber = 12;
-var testString =
-`\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum
-Lorem ipsum
-Lorem ipsum
-Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum`
-// starts with code block, rest is plain text
-var textShouldBe = 
-`\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum
-Lorem ipsum
-Lorem ipsum
-Lorem ipsum
-lorem ipsum
-lorem ipsum
-lorem ipsum
-lorem ipsum`
-        var modifiedShouldBe = true;
-        // test
-        var result = wrapText(testString, givenNumber);
-        expect(result.s).toBe(textShouldBe);
-        expect(result.modified).toBe(modifiedShouldBe);
-     }); 
-
-
-     it("Shouldn't wrap code block insides, but should wrap the texts before and after it", () => {
-        // assertions
-        var givenNumber = 12;
-var testString = 
-`Lorem ipsum
-Lorem ipsum
-Lorem ipsum
-Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\``;
-// starts with plain text, ends in code block
-var textShouldBe = 
-`Lorem ipsum
-Lorem ipsum
-Lorem ipsum
-Lorem ipsum
-lorem ipsum
-lorem ipsum
-lorem ipsum
-lorem ipsum
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\``;
-        var modifiedShouldBe = true;
-        // test
-        var result = wrapText(testString, givenNumber);
-        expect(result.s).toBe(textShouldBe);
-        expect(result.modified).toBe(modifiedShouldBe);
-     }); 
-
-
      it("Shouldn't wrap simultaneous code blocks", () => {
         // assertions
         var givenNumber = 12;
-var testString = 
+        var testString = 
 `\`\`\`
     print("hello world hello world hello world")
 \`\`\`
@@ -233,76 +199,76 @@ var testString =
 \`\`\`
     print("hello world hello world hello world")
 \`\`\``
-// multiple code blocks, doesn't need wrapping
-var textShouldBe = testString.slice();
+        // multiple code blocks, doesn't need wrapping
+        var textShouldBe = testString.slice();
         var modifiedShouldBe = false;
         // test
         var result = wrapText(testString, givenNumber);
         expect(result.s).toBe(textShouldBe);
         expect(result.modified).toBe(modifiedShouldBe);
-     }); 
+     });
 
 
-     it("Shouldn't wrap code block insides, but should wrap the texts before and after it", () => {
+     it("Should wrap as described", () => {
         // assertions
-        var givenNumber = 12;
-var testString =
-`Lorem ipsum lorem ipsum lorem ipsum
+        var givenNumber = 25;
+        var testString = 
+`Vulputate dignissim suspendisse in est ante in. Sit amet massa vitae tortor condimentum.
+$$
+2x+5 - 6547 + 1254645y - 1244324z + 31532 = 0
+this math block's insides shouldn't be affected.
+$$
 \`\`\`
+print("hello world hello world hello world")
+print("hello world hello world hello world")
+\`\`\`
+Congue quisque egestas diam in arcu. Ante metus dictum at tempor commodo ullamcorper a lacus vestibulum.
     print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum lorem ipsum lorem ipsum`
-// Code block inside wrappable lines
-var textShouldBe = 
-`Lorem ipsum
-lorem ipsum
-lorem ipsum
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum
-lorem ipsum
-lorem ipsum`
-        var modifiedShouldBe = true;
-        // test
-        var result = wrapText(testString, givenNumber);
-        expect(result.s).toBe(textShouldBe);
-        expect(result.modified).toBe(modifiedShouldBe);
-     }); 
+List of favourite animals. The list shouldn't be wrapped but the text before it should.
+- Cat
+   - Birman
+   - Maine Coon
+      - Peruvian
+   - Sphynx
+- Lynx
+- Cheetah
 
+Should - wrap - this - even - through - there - are - hyphens`;
+        var textShouldBe =
+`Vulputate dignissim
+suspendisse in est ante
+in. Sit amet massa vitae
+tortor condimentum.
+$$
+2x+5 - 6547 + 1254645y - 1244324z + 31532 = 0
+this math block's insides shouldn't be affected.
+$$
+\`\`\`
+print("hello world hello world hello world")
+print("hello world hello world hello world")
+\`\`\`
+Congue quisque egestas
+diam in arcu. Ante metus
+dictum at tempor commodo
+ullamcorper a lacus
+vestibulum.
+    print("hello world hello world hello world")
+List of favourite
+animals. The list
+shouldn't be wrapped but
+the text before it
+should.
+- Cat
+   - Birman
+   - Maine Coon
+      - Peruvian
+   - Sphynx
+- Lynx
+- Cheetah
 
-     it("Shouldn't wrap code block insides, but should wrap the texts before and after it", () => {
-        // assertions
-        var givenNumber = 12;
-var testString = 
-`\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum lorem ipsum lorem ipsum
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum lorem ipsum lorem ipsum`
-// Two simultaneous code blocks in the beginning, wrappable text and one code block in the middle
-var textShouldBe = 
-`\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum
-lorem ipsum
-lorem ipsum
-\`\`\`
-    print("hello world hello world hello world")
-\`\`\`
-Lorem ipsum
-lorem ipsum
-lorem ipsum`
+Should - wrap - this -
+even - through - there -
+are - hyphens`;
         var modifiedShouldBe = true;
         // test
         var result = wrapText(testString, givenNumber);
